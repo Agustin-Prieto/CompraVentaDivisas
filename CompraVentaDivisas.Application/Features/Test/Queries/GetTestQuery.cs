f18d41cba11eadaa10fa61e3d27800adf4972874
agustin prieto <agustinprieto.ar@gmail.com>
2024-02-10 00:54:22 -0300

    Test deleted

--- CompraVentaDivisas.Application/Features/Test/Queries/GetTestQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using CompraVentaDivisas.Application.Abastractions.Messaging;
-using CompraVentaDivisas.Domain.Entities;
-
-namespace CompraVentaDivisas.Application.Features.Test.Queries;
-
-public sealed record GetTestQuery(Guid Id) : IQuery<TestEntity>;
--- CompraVentaDivisas.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CompraVentaDivisas.Domain
-{
-    public class Class1
-    {
-
-    }
-}
