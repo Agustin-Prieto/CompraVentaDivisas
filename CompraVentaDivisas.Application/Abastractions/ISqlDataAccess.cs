c0e8b06fdab1d5740182fe08f90fc311df6e9de6
agustin prieto <agustinprieto.ar@gmail.com>
2024-02-10 00:55:11 -0300

    SqlDataAcces abstraction

---
+++ CompraVentaDivisas.Application/Abastractions/ISqlDataAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace CompraVentaDivisas.Application.Abastractions;
+
+public interface ISqlDataAccess
+{
+    Task<IEnumerable<T>> LoadData<T, U>(string sql, U parameters);
+
+    Task SaveData<T>(string sql, T parameters);
+}
---
+++ CompraVentaDivisas.Infrastructure/ContextConfig/SqlDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using CompraVentaDivisas.Application.Abastractions;
+using Dapper;
+using System.Data;
+
+namespace CompraVentaDivisas.Infrastructure.ContextConfig;
+
+public sealed class SqlDataAccess : ISqlDataAccess
+{
+    private readonly SqlConnectionFactory _sqlConnectionFactory;
+
+    public SqlDataAccess(SqlConnectionFactory sqlConnectionFactory)
+    {
+        _sqlConnectionFactory = sqlConnectionFactory;
+    }
+
+    public async Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters)
+    {
+        using var sqlConnection = _sqlConnectionFactory.CreateConnection();
+
+        return await sqlConnection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+
+    public async Task SaveData<T>(string storedProcedure, T parameters)
+    {
+        using var sqlConnection = _sqlConnectionFactory.CreateConnection();
+
+        await sqlConnection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+    }
+}
