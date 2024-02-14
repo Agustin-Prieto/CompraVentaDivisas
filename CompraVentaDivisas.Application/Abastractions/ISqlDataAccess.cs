938360be5fba5446b44f1788948f0e90d9bbe94d
agustin prieto <agustinprieto.ar@gmail.com>
2024-02-14 18:33:47 -0300

    CreateConnection method exposed

--- CompraVentaDivisas.Application/Abastractions/ISqlDataAccess.cs
+++ CompraVentaDivisas.Application/Abastractions/ISqlDataAccess.cs
@@ -1,7 +1,10 @@
-﻿namespace CompraVentaDivisas.Application.Abastractions;
+﻿using Microsoft.Data.SqlClient;
+
+namespace CompraVentaDivisas.Application.Abastractions;
 
 public interface ISqlDataAccess
 {
+    SqlConnection CreateConnection();
     Task<IEnumerable<T>> LoadData<T, U>(string sql, U parameters);
     Task<T> LoadDataSingle<T, U>(string sql, U parameters);
 
